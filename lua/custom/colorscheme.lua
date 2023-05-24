2cc6bce14da6af98f37d52f54b2594339adb8254
Keith Barratt <k.barratt@systemloco.com>
2023-05-24 20:56:51 +0100

    Added ranger- started some other mods

---
+++ lua/custom/colorscheme.lua
@@ -0,0 +1,15 @@
+
+
+
+
+
+return {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  }
+
+
