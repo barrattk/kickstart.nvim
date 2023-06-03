8282f608c9bed69b2ede4152594f182129b86ccb
Keith Barratt <k.barratt@systemloco.com>
2023-06-03 16:25:40 +0100

    More mods - python DAP now works

--- init.lua
+++ init.lua
@@ -341,10 +341,10 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<M-space>', -- This is captured by Ubuntu
+      init_selection = '<ENTER>',
+      node_incremental = '<ENTER>',
+      scope_incremental = '<c-s>', -- Not working?
+      node_decremental = '<BS>',
     },
   },
   textobjects = {
@@ -420,9 +420,12 @@ local on_attach = function(_, bufnr)
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
@@ -431,7 +434,6 @@ local on_attach = function(_, bufnr)
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
   nmap('<leader>wl', function()
@@ -451,7 +453,7 @@ end
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   clangd = {  --- I've got no idea if these are being picked up
-      filetypes ={ "keith" },
+      -- filetypes ={ "keith" },
       single_file_support = false,
   },
   --  gopls = {},
