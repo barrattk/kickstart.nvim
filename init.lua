5f448f95cc73f43cc9fdc09f6ff685924aafb706
Keith Barratt <k.barratt@systemloco.com>
2023-06-06 14:30:54 +0100

    Tidying up

--- init.lua
+++ init.lua
@@ -1,43 +1,10 @@
--- --[[
---
--- =====================================================================
--- ==================== READ THIS BEFORE CONTINUING ====================
--- =====================================================================
---
--- Kickstart.nvim is *not* a distribution.
---
--- Kickstart.nvim is a template for your own configuration.
---   The goal is that you can read every line of code, top-to-bottom, understand
---   what your configuration is doing, and modify it to suit your needs.
---
---   Once you've done that, you should start exploring, configuring and tinkering to
---   explore Neovim!
---
---   If you don't know anything about Lua, I recommend taking some time to read through
---   a guide. One possible example:
---   - https://learnxinyminutes.com/docs/lua/
---
---   And then you can explore or search through `:help lua-guide`
---
---
--- Kickstart Guide:
---
--- I have left several `:help X` comments throughout the init.lua
--- You should run that command and read that help section for more information.
---
--- In addition, I have some `NOTE:` items throughout the file.
--- These are for you, the reader to help understand what is happening. Feel free to delete
--- them once you know what you're doing, but they should serve as a guide for when you
--- are first encountering a few different constructs in your nvim config.
---
--- I hope you enjoy your Neovim journey,
--- - TJ
---
--- P.S. You can delete this when you're done too. It's your config now :)
--- --]]
--- -- Set <space> as the leader key
--- -- See `:help mapleader`
--- --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+
+
+-- The starting point was https://github.com/nvim-lua/kickstart.nvim
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+-- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 
 
 vim.g.mapleader = ' '
@@ -76,6 +43,7 @@ require('lazy').setup({
   'tpope/vim-sleuth',
   'kelly-lin/ranger.nvim',
 
+  require 'colorscheme',
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -136,12 +104,6 @@ require('lazy').setup({
   },
 
 
-
-  require 'colorscheme',
-
-
-
-
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -188,15 +150,12 @@ require('lazy').setup({
     end,
   },
 
-
-
-    {
-    "ThePrimeagen/refactoring.nvim",
-    dependancies = {
-        {"nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter"}
-      }
-    },
+  {  "ThePrimeagen/refactoring.nvim",
+  dependancies = {
+      {"nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter"}
+    }
+  },
 
   {
     -- Highlight, edit, and navigate code
@@ -214,9 +173,7 @@ require('lazy').setup({
       'nvim-telescope/telescope.nvim'
     },
   },
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
+
   -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
   require 'dap_debug',
