1888118df3f867912476637f066b8ad2173beda0
Keith Barratt <k.barratt@systemloco.com>
2024-02-28 21:38:55 +0000

    Gitsigns reconfigured

--- init.lua
+++ init.lua
@@ -41,8 +41,6 @@ require('lazy').setup({
   'tpope/vim-rhubarb',
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
-  -- ranger
-  -- 'kelly-lin/ranger.nvim',
   -- Mason
   'williamboman/mason.nvim',
   -- Buffer delete
@@ -56,8 +54,7 @@ require('lazy').setup({
   require 'comment',
   require 'ranger',
 
-
-  -- require 'term',
+ -- require 'term',
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -91,32 +88,8 @@ require('lazy').setup({
       {'L3MON4D3/LuaSnip'},     -- Required
     }
   },
-  -- require 'gitsigns',
 
   'lewis6991/gitsigns.nvim',
-  {
-    -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      -- signs = {
-      --   add = { text = '|' },
-      --   change = { text = '|' },
-      --   delete = { text = '_' },
-      --   topdelete = { text = '‾' },
-      --   changedelete = { text = '~' },
-      --   untracked = { text = '┆' },
-      -- },
-      on_attach = function(bufnr)
-        -- vim.keymap.set('n', '[c', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Go to Previous Hunk' })
-        -- vim.keymap.set('n', ']c', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Go to Next Hunk' })
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-      end,
-    },
-  },
-
 
   {
     -- Set lualine as statusline
@@ -136,15 +109,6 @@ require('lazy').setup({
   -- Add indentation guides even on blank lines
   "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}
   },
-  -- {
-  --   'lukas-reineke/indent-blankline.nvim',
-  --   -- Enable `lukas-reineke/indent-blankline.nvim`
-  --   -- See `:help indent_blankline.txt`
-  --   opts = {
-  --     char = '┊',
-  --     show_trailing_blankline_indent = false,
-  --   },
-  -- },
 
   -- Fuzzy Finder (files, lsp, etc)
   {'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
@@ -214,6 +178,54 @@ require('lazy').setup({
 
 }, {})
 
+-----------------------------------------------------------------------------------
+-- End lazy setup
+-----------------------------------------------------------------------------------
+
+
+require('gitsigns').setup{
+--  ...
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage Hunk'})
+    map('n', '<leader>hr', gs.reset_hunk, { desc = 'Reset Hunk'})
+    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Stage Hunk'})
+    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Reset Hunk'})
+    map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage buffer'})
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo_stage_hunk'})
+    map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset buffer'})
+    map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Hunk'})
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = 'Blame Line'})
+    map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'Toggle blame_line'})
+    map('n', '<leader>hd', gs.diffthis, { desc = 'Diff this'})
+    map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'Diff this'})
+    map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle_deleted'})
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
 
 
 -- [[ Highlight on yank ]]
