2347d3f9d14e4a86758046b0abf3cc36ef23543f
Keith Barratt <k.barratt@systemloco.com>
2024-12-31 10:14:56 +0000

    flash-cfg.lua added and used

--- init.lua
+++ init.lua
@@ -137,21 +137,7 @@ require('lazy').setup({
   -- Markup Viewer
   {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
 
-  -- require 'flash',
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    },
-  },
+  require 'flash-cfg',
 
   -- Project Extension in Telescope
   -- I don't really understand how to use this project plugin
