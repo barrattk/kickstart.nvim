8e0e038135374f2423697a8a92a1f2e14b0c2427
Keith Barratt <k.barratt@systemloco.com>
2025-01-01 11:27:52 +0000

    Moved telescope

--- init.lua
+++ init.lua
@@ -101,28 +101,8 @@ require('lazy').setup({
   "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}
   },
 
-  -- Fuzzy Finder (files, lsp, etc)
-  {'nvim-telescope/telescope.nvim',
-    -- branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    file_ignore_patterns = {"_build"}
-  },
+  require 'config.telescope',
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
-  -- {
-  --   'nvim-telescope/telescope-fzf-native.nvim',
-  --   -- NOTE: If you are having trouble with this installation,
-  --   --       refer to the README for telescope-fzf-native for more instructions.
-  --   build = 'make',
-  --   cond = function()
-  --     return vim.fn.executable 'make' == 1
-  --   end,
-  -- },
-
-  { 'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
@@ -137,14 +117,6 @@ require('lazy').setup({
 
   require 'flash-cfg',
 
-  -- Project Extension in Telescope
-  -- I don't really understand how to use this project plugin
-  -- {
-  --    'ahmedkhalf/project.nvim',
-  --     dependencies = {
-  --     'nvim-telescope/telescope.nvim'
-  --   },
-  -- },
 
   -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
@@ -192,59 +164,8 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   command = [[%s/\s\+$//e ]],
 })
 
---
--- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    -- vim grep arguments ...
-    -- https://github.com/nvim-telescope/telescope.nvim/issues/470#issuecomment-767904334
-    -- this applies to all Telescope grep calls
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '-uu', -- Unrestricted !-- two -u flags means search hidden files and directories "--hidden" also works
-      '-L', -- follow symoblic links
-    },
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
--- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep{vimgrep_arguments = vimgrep_args_live}, { desc = '[S]earch by [G]rep' })
-
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 require "vim-options"
--- require("project_nvim").setup{}
--- require('telescope').load_extension('projects')  -- Type :Telescope projects
 require 'keymappings'
 require 'commands'
 require 'rainbow-delimiters'
