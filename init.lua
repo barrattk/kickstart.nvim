7c7b25bbe6d1222a98b5bed19b2e34f60a8b5616
Keith Barratt <k.barratt@systemloco.com>
2025-01-05 16:57:00 +0000

    move lsp from lazy

--- init.lua
+++ init.lua
@@ -19,33 +19,6 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require('lazy').setup({
-
-  -- Mason
-  'williamboman/mason.nvim',
-
-  -- 'p00f/clangd_extensions.nvim',
-
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  -- The configuration is done below. Search for lspconfig to find it below.
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      -- { 'j-hui/fidget.nvim', branch ='legacy', tag = 'legacy', commit= '0ba1e16', opts = {} },
-      { 'j-hui/fidget.nvim',       opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-      'saghen/blink.cmp'
-    },
-  },
-
   -- For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- import everything in the lua/config folder
   { import = 'config' },
